ad6611f62ce70c4a809d8f8165c9d1f90bcf5f1e
ibktommy <atomquartz@gmail.com>
2022-07-10 22:30:44 +0100

    Structuring the jsx

--- src/App.js
+++ src/App.js
@@ -3,7 +3,48 @@ import './App.css';
 function App() {
   return (
     <>
-      <h1>Tabs Project</h1>
+      <div className="container">
+        <div className="content">
+          <h2 className="content-title">
+            Experience
+          </h2>
+          <div className="content-flex">
+            <div className="left">
+              <buuton className="btn">Tommy</buuton>
+              <buuton className="btn">Bigdrop</buuton>
+              <buuton className="btn">Cuker</buuton>
+            </div>
+            <div className="right">
+              <h4 className="header">Engineering Intern</h4>
+              <p className="company">Cuker</p>
+              <p className="period">May 2014 - September 2015</p>
+              <ul className="worklist">
+                <li>
+                  <i class="fa-solid fa-chevrons-right"></i>
+                  <p className="work-details">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident, labore. Quo, dolores commodi. Repellat, dicta.
+                  </p>
+                </li>
+                <li>
+                  <i class="fa-solid fa-chevrons-right"></i>
+                  <p className="work-details">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident, labore. Quo, dolores commodi. Repellat, dicta.
+                  </p>
+                </li>
+                <li>
+                  <i class="fa-solid fa-chevrons-right"></i>
+                  <p className="work-details">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident, labore. Quo, dolores commodi. Repellat, dicta.
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="content-btn">
+            <button>More info</button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
