9c240ed8f7ca2fe862db816bb240026a390808b0
ibktommy <atomquartz@gmail.com>
2022-07-11 21:59:36 +0100

    Using UseEffect Hook to effect change when we fetch the Data

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import './App.css';
 import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
+import { useEffect, useState } from 'react';
 
 // Database
 const url = 'https://course-api.com/react-tabs-project'
 
-// Function to get Data
-const fetchURL = async () => {
-  const response = await fetch(url)
-  const responseData = await response.json()
-
-  console.log(responseData)
-}
-
-fetchURL()
-
 function App() {
+  // Setting state
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState([])
+
+
+  // Function to get Data
+  const fetchURL = async () => {
+    const response = await fetch(url)
+    const responseData = await response.json()
+
+    setData(responseData)
+    setLoading(false)
+  }
+
+  // Setting UseEffect For Event Listener on the Data being fetched
+  useEffect(() => {
+    fetchURL()
+  }, [])
+
+  if (loading) {
+    return (
+      <>
+        <h1 className='title-center'>Loading Data...</h1>
+      </>
+    )
+  }
+
   return (
     <>
       <div className="container">
